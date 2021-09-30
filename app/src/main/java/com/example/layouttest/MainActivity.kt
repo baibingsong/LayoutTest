909fcc64449c13002923c872411c577d33ec9ad2
qianzhisanyao <bsong@miu.edu>
2021-09-29 21:47:42 -0700

    Initial commit

---
+++ app/src/main/java/com/example/layouttest/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.layouttest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TableRow
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onAdd(view: View) {
+
+        var version: String?
+        var codeName: String?
+        var isAdd: Boolean = true
+
+        version = editVersion.text.toString()
+        codeName = editCodeName.text.toString()
+
+        if(version.isEmpty()) {
+            Toast.makeText(this, "Enter the version", Toast.LENGTH_LONG).show()
+            isAdd = false
+        }
+
+        if(codeName.isEmpty()) {
+            Toast.makeText(this, "Enter the code name", Toast.LENGTH_LONG).show()
+            isAdd = false
+        }
+
+        if(isAdd){
+            val tableRow = TableRow(getApplicationContext())
+
+            val layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT)
+            tableRow.setLayoutParams(layoutParams)
+
+            val text_version = TextView(this)
+            text_version.text = version
+            tableRow.addView(text_version)
+
+            val text_codename = TextView(this)
+            text_codename.text = codeName
+            tableRow.addView(text_codename)
+
+            tblDynamic.addView(tableRow)
+
+            Toast.makeText(this, "Added the row", Toast.LENGTH_LONG).show()
+
+            editVersion.text.clear()
+            editCodeName.text.clear()
+        }
+    }
+}
